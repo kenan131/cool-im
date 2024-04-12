28abadf9b72d0a4a791b6e6b121efee71854962b
[bin] <[1020195983@qq.com]>
2024-04-12 17:12:04 +0800

    初始化项目

---
+++ im/src/main/java/com/bin/im/InstantMessageApplication.java
@@ -0,0 +1,13 @@
+package com.bin.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstantMessageApplication.class, args);
+    }
+
+}
---
+++ im/src/test/java/com/bin/im/InstantMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bin.im;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
