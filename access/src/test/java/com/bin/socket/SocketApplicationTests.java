e2e5676201c18f21ebca3c1f8de87a8708def6c4
[bin] <[1020195983@qq.com]>
2024-06-05 11:18:43 +0800

    init

---
+++ access/src/test/java/com/bin/socket/SocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bin.socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ router/src/test/java/com/example/router/RouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
